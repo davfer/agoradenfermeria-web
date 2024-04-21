5b51d33f584eea9eb52b5b1839825fc6f0d165fd
david <paido2005@gmail.com>
2024-04-21 17:58:10 +0200

    api and definitions

--- src/models/Search.ts
+++ src/models/Search.ts
@@ -1,5 +1,27 @@
-interface Search {
-  magazine: Magazine
-  summary: Summary
 
+export interface SearchResults {
+  result: SearchResult[];
+  total: number;
+  time: number;
+  page: number;
+}
+export interface SearchResult {
+
+  summary_id: string;
+  language: string;
+  title: string;
+  subtitle: string;
+  page: string;
+  keywords: string;
+  id: string;
+  magazine_id: string;
+  section_id: string;
+  position: string;
+
+  authors: {
+    id: string;
+    shown: string;
+  }[];
+  magazine: Magazine;
+  section: SummarySection;
 }
--- src/pages/search.vue
+++ src/pages/search.vue
@@ -2,4 +2,34 @@
   SEARCH
 </template>
 <script lang="ts" setup>
+
+import {onMounted} from "vue";
+import {useRoute, useRouter} from "vue-router";
+import {useFetcher} from "@/lib/fetcher";
+import {SearchResult, SearchResults} from "@/models/Search";
+
+const result = ref<SearchResults>()
+const loading = ref(false)
+
+const fetcher = useFetcher()
+const route = useRoute()
+onMounted(() => {
+  console.log("Search page: ", route.query)
+
+  fetchSearch(route.query.q, route.query.p)
+})
+
+const fetchSearch = async (query: string, page: number) => {
+  loading.value = true
+  const params = new URLSearchParams()
+  params.append('q', query)
+  if (page) {
+    params.append('p', page.toString())
+  }
+
+  const res = await fetcher.get(`search?${params.toString()}`)
+  result.value = res.data
+  loading.value = false
+  console.log("Search results: ", result.value)
+}
 </script>
